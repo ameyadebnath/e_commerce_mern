6dd8024e5aeb62c496316f6b0e9c7e03504dda86
Shrestha Datta <52985769+Shrestha30@users.noreply.github.com>
2023-06-11 15:07:47 +0600

    webpage not showing error solve

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -8,13 +8,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
-      </BrowserRouter>
+      
       {/* <Login /> */}
     </div>
   );
--- e-commerce/src/index.js
+++ e-commerce/src/index.js
@@ -3,13 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {  BrowserRouter, Route, Routes } from 'react-router-dom';
+import Homepage from "./components/homepage/homepage";
+import Login from "./components/login/login";
+import Register from "./components/register/register";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
  
-    <App />
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
   
 )
+
+reportWebVitals();
 
 
 
