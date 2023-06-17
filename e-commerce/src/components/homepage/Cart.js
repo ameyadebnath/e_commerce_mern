0f89c5dbbd76f82b00e9a098d961cf398fd63b27
ameya <52748765+ameyadebnath@users.noreply.github.com>
2023-06-17 15:35:57 +0600

    Update Cart.js
    done

--- e-commerce/src/components/homepage/Cart.js
+++ e-commerce/src/components/homepage/Cart.js
@@ -13,29 +13,18 @@ const Cart = ({
   handleRemoveProduct,
   handleCartClearance,
 }) => {
-<<<<<<< Updated upstream
   const [balance, setBalance] = useState("");
-=======
-
-  const [balance, setBalance] = useState('');
->>>>>>> Stashed changes
 
   useEffect(() => {
     // Function to fetch the user's account balance
     const fetchAccountBalance = async () => {
       try {
-<<<<<<< Updated upstream
         const response = await axios.post(
           "http://localhost:9003/getUserAmount",
           {
             bankId: user.bankid, // Replace with the actual user bank ID
           }
         );
-=======
-        const response = await axios.post('http://localhost:9003/getUserAmount', {
-          bankId: user.bankid, // Replace with the actual user bank ID
-        });
->>>>>>> Stashed changes
         const { amount, success } = response.data;
         if (success) {
           setBalance(amount);
@@ -59,7 +48,6 @@ const Cart = ({
   );
 
   var makeOrderRequest = () => {
-<<<<<<< Updated upstream
     console.log(cartItems);
     console.log(user._id);
     axios
@@ -78,22 +66,6 @@ const Cart = ({
         // Handle any errors
       });
   };
-=======
-    console.log(cartItems)
-    console.log(user._id)
-    axios.post('http://localhost:9002/placeOrder', {orders: cartItems, userId: user._id})
-    .then(response => {
-      console.log(response.data);
-      if(response.data.success===1) toast.success(response.data.message);
-      else toast.warning(response.data.message);
-      // Process the response data
-    })
-    .catch(error => {
-      console.log(error);
-      // Handle any errors
-    });
-  }
->>>>>>> Stashed changes
 
   return (
     <div>
@@ -187,14 +159,10 @@ const Cart = ({
         </div>
       </div>
       <div className="container d-flex justify-content-center align-items-center">
-<<<<<<< Updated upstream
         <button className="submit" onClick={makeOrderRequest}>
           {" "}
           Auto-Pay{" "}
         </button>
-=======
-        <button className="submit" onClick={makeOrderRequest}> Auto-Pay </button>
->>>>>>> Stashed changes
       </div>
     </div>
   );
